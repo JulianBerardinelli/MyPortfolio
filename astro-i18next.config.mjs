c3962f62ee47ccdcc7c1f7970da1f7538b4a333a
Julián Berardinelli <jberardinelli@MacBook-Pro-de-Julian.local>
2025-07-10 14:47:49 +0300

    Actualizacion cv

--- astro-i18next.config.mjs
+++ astro-i18next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('astro-i18next').AstroI18nextConfig} */
 export default {
     defaultLocale: "en",
-    locales: ["en", "es", "pt", "it", "de"],
+    locales: ["en", "es", "pt", "it", "de", "fi"],
     // currentLocale: process.env.LOCALE || navigator.language.slice(0, 2), 
   };
